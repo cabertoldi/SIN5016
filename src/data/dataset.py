ca512e98309d2c89a06d91087e2945fa287097d4
ale <0800alefarias@gmail.com>
2023-01-12 20:55:53 -0300

    Adiciona dataset de imagens com train_test_split

---
+++ src/data/dataset.py
@@ -0,0 +1,112 @@
+import gc
+
+import pandas as pd
+
+import torch
+from torch.utils.data import Dataset, Subset
+import torchvision as tvis
+
+from sklearn.model_selection import ShuffleSplit
+
+from loguru import logger
+
+
+BAD_IMAGES = [
+    'Abdullah_al-Attiyah_0001.jpg',
+    'Dereck_Whittenburg_0001.jpg',
+    'Lawrence_Foley_0001.jpg'
+]
+
+
+def train_test_split(dataset: Dataset, test_size: float):
+    split_generator = ShuffleSplit(n_splits=1, test_size=test_size).split(dataset)
+    train_idx, test_idx = list(split_generator)[0]
+
+    train_dataset = Subset(dataset=dataset, indices=train_idx)
+    test_dataset = Subset(dataset=dataset, indices=test_idx)
+
+
+    return train_dataset, test_dataset
+
+
+
+class LFWDataset(Dataset):
+    
+    def __init__(self, path: str, preprocessed_prefix: str):
+        self.data = []
+        self.data_tuples = None
+        self.path = path
+        self.preprocessed_prefix = preprocessed_prefix
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        # returns x1, x2, y
+        return self.data[idx]
+
+    def filter_bad_images(self, df):
+        return df[
+            ~df.img1_full_id.isin(BAD_IMAGES) &
+            ~df.img2_full_id.isin(BAD_IMAGES)
+        ] 
+    
+    def load_data(self):
+        self.load_df()
+        self.load_images()
+    
+    def load_df(self):
+        logger.info("Loading pairs dataframe")
+        df = pd.read_parquet(self.path).pipe(self.filter_bad_images)
+
+        df["prepro_img1"] = df.img1_full_id.apply(lambda s: f"{self.preprocessed_prefix}/{s}")
+        df["prepro_img2"] = df.img2_full_id.apply(lambda s: f"{self.preprocessed_prefix}/{s}")
+
+        self.data_tuples = df[["prepro_img1", "prepro_img2", "match"]].values.copy()
+        del df
+    
+    def load_images(self):
+        logger.info("Loading images")
+
+        for im1_path, im2_path, match in self.data_tuples:
+            im1 = tvis.io.read_image(im1_path).float()
+            im2 = tvis.io.read_image(im2_path).float()
+            self.data.append((im1, im2, torch.as_tensor(match).float()))
+    
+    def to_cuda(self):
+        if not torch.cuda.is_available():
+            raise Exception("CUDA isn't available.")
+        
+        logger.info("Loading dataset to CUDA")
+
+        new_data = []
+
+        for im1, im2, target in self.data:
+            im1 = im1.to("cuda")
+            im2 = im2.to("cuda")
+            target = target.to("cuda")
+
+            new_data.append((im1, im2, target))
+        
+        gc.collect()
+        self.data = new_data
+    
+    def free(self):
+        for im1, im2, target in self.data:
+            im1.to("cpu")
+            im2.to("cpu")
+            target.to("cpu")
+
+            del im1
+            del im2
+            del target
+
+        self.data = []
+        torch.cuda.empty_cache()
+        gc.collect()
+
+
+LFW_DATASET = LFWDataset(
+    path="data/interim/pairs.parquet",
+    preprocessed_prefix="data/preprocessed/images",
+)
