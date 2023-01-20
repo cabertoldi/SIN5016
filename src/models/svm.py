4b4db0b3aeaebca7824f067612c80643f80f11df
ale <0800alefarias@gmail.com>
2023-01-20 12:25:52 -0300

    Refatoração do modelo SVM

--- src/models/svm.py
+++ src/models/svm.py
@@ -8,11 +8,10 @@ from cvxopt import matrix as cvxopt_matrix
 from cvxopt import solvers as cvxopt_solvers
 from loguru import logger
 from sklearn.base import BaseEstimator
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import make_classification
 
 
 class InvalidTargetException(Exception):
@@ -32,14 +31,14 @@ def polynomial_kernel(x, y, d=3):
 # noinspection PyMethodOverriding
 class SVM(BaseEstimator):
     def __init__(
-        self, C: float = 10, kernel: Callable = linear_kernel, degree: int = None
+        self, C: int = 10, kernel: Callable = linear_kernel, degree: int = None
     ):
         self.C = C
         self.degree = degree
         self.kernel = kernel
         self.kernel_name = kernel.__name__
 
-        # parametros do modelo
+        # parâmetros do modelo
         self.support_vectors = None
         self.support_vectors_y = None
         self.bias = None
@@ -50,11 +49,16 @@ class SVM(BaseEstimator):
     def validate_target(y: np.array):
         """Variável alvo deve ser binária, codificada como 1 e -1"""
         unique_values = set(np.unique(y.astype(int)))
-        return all(u in {1, -1} for u in unique_values)
+        if all(u in {1, -1} for u in unique_values):
+            return True
+        raise InvalidTargetException(
+            "Variável alvo deve ser binária, codificada como 1 e -1"
+        )
 
-    def solve_qp(self, k: np.array, x: np.array, y: np.array):
+    def solve_qp(self, x: np.array, y: np.array):
         n_samples, _ = x.shape
 
+        k = self.kernel(x, x.T)
         p = cvxopt_matrix(np.outer(y, y) * k)
         q = cvxopt_matrix(-np.ones(n_samples))
         g = cvxopt_matrix(np.vstack((-np.eye(n_samples), np.eye(n_samples))))
@@ -67,24 +71,16 @@ class SVM(BaseEstimator):
 
         support_idx = alphas > 1e-4
 
-        ind = np.arange(len(alphas))[
-            support_idx
-        ]  # lista com indices dos vetores suporte
+        lagrange_multipliers = alphas[support_idx]
+        support_vectors = x[support_idx]
+        support_vectors_y = y[support_idx]
 
-        self.lagrange_multipliers = alphas[support_idx]
-        self.support_vectors = x[support_idx]
-        self.support_vectors_y = y[support_idx]
+        return lagrange_multipliers, support_vectors, support_vectors_y
 
-        logger.info("calculating bias")
-
-        logger.info("calculating w")
-        self.w = (
-            self.lagrange_multipliers * self.support_vectors_y @ self.support_vectors
-        )
-        self.bias = np.median(self.support_vectors_y - (self.support_vectors @ self.w))
-
+    def estimate_bias(self):
+        """Calcula o bias"""
         if self.kernel_name == "polynomial_kernel":
-            self.bias = np.mean(
+            bias = np.mean(
                 self.support_vectors_y
                 - sum(
                     self.lagrange_multipliers
@@ -92,29 +88,39 @@ class SVM(BaseEstimator):
                     * self.kernel(self.support_vectors, self.support_vectors.T)
                 )
             )
+        elif self.kernel_name == "linear_kernel":
+            bias = np.median(self.support_vectors_y - (self.support_vectors @ self.w))
+        return bias
 
-        return self.w, self.bias
+    def estimate_coefficients(self):
+        """Calcula os coeficientes para o caso do kernel linear"""
+        return self.lagrange_multipliers * self.support_vectors_y @ self.support_vectors
 
-    def fit(self, x, y):
-        logger.info(
-            f"Fitting SVC with params: C = {self.C}, kernel = {self.kernel_name}, d = {self.degree}"
-        )
-
-        if not self.validate_target(y):
-            raise InvalidTargetException("Targets devem estar codificados como -1 e 1")
-
+    def setup_kernel(self):
+        """Realiza operações de configuração específicas para cada kernel"""
         if self.kernel_name == "polynomial_kernel":
             self.kernel = partial(self.kernel, d=self.degree)
 
-        n_samples, n_features = x.shape
+    def fit(self, x, y):
+        """Ajusta os parâmetros do modelo"""
+        logger.info(f"Fitting model {self}")
+        self.setup_kernel()
 
-        # Gram matrix
-        k = self.kernel(x, x.T)
-        self.solve_qp(k, x, y)
+        SVM.validate_target(y)
+
+        (
+            self.lagrange_multipliers,
+            self.support_vectors,
+            self.support_vectors_y,
+        ) = self.solve_qp(x, y)
+
+        self.w = self.estimate_coefficients()
+        self.bias = self.estimate_bias()
 
         return self
 
     def predict(self, x):
+        """Recebe novos dados de entrada e retorna as predições {1, -1}"""
 
         if self.kernel_name == "linear_kernel":
             return np.sign(x @ self.w + self.bias)
@@ -129,6 +135,7 @@ class SVM(BaseEstimator):
         return np.sign(y_pred + self.bias)
 
     def score(self, x: np.array, y: np.array) -> float:
+        """Recebe um conjunto de dados x e a variável y e retorna a acurácia do modelo neste conjunto"""
         y_pred = self.predict(x)
         return np.sum(y == y_pred) / len(y)
 
@@ -151,26 +158,11 @@ def load_hog():
     return load_dataset("data/preprocessed/feature_matrix_hog.parquet")
 
 
-def test():
-    x, y = make_classification(n_samples=100, n_features=2, n_redundant=0)
-    y = 2 * y - 1
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
-    clf = SVM(C=10, kernel=polynomial_kernel, degree=3)
-    clf.fit(x_train, y_train)
-
-    y_pred = clf.predict(x_test)
-
-    print(confusion_matrix(y_test.astype(np.int64), y_pred.astype(np.int64)))
-    print(classification_report(y_test.astype(np.int64), y_pred.astype(np.int64)))
-
-
 def make_cross_val(x, y):
     params = {
-        "clf__C": [1, 10, 100, 1000],
+        "clf__C": [10, 100, 1000],
         "clf__degree": [2, 3, 4],
-        "clf__kernel": [linear_kernel],
+        "clf__kernel": [linear_kernel, polynomial_kernel],
     }
 
     est = Pipeline([("scl", StandardScaler()), ("clf", SVM())])
@@ -188,7 +180,9 @@ def save_model_results(
     classification_report_text: str,
     results: Dict,
     model: BaseEstimator,
+    preds_df: pd.DataFrame,
 ):
+    """Salva resultados dos experimentos"""
     with open(f"execucao/{dataset_name}-classification_report.txt", "w") as f:
         f.write(classification_report_text)
 
@@ -198,8 +192,11 @@ def save_model_results(
     with open(f"execucao/{dataset_name}-model.pkl", "wb") as f:
         pickle.dump(model, f)
 
+    preds_df.to_csv(f"execucao/{dataset_name}-preds.csv", index=False)
+
 
 def main():
+    """Executa toda pipeline de treinamento para todos os conjuntos de dados"""
     datasets = [("LBP", load_lbp), ("HOG", load_lbp)]
 
     for dataset_name, loader in datasets:
@@ -212,8 +209,9 @@ def main():
         y_pred = best_model.predict(x_test)
 
         class_report = classification_report(y_test, y_pred)
+        preds_df = pd.DataFrame({"y_test": y_test, "y_pred": y_pred})
 
-        save_model_results(dataset_name, class_report, results, best_model)
+        save_model_results(dataset_name, class_report, results, best_model, preds_df)
 
 
 if __name__ == "__main__":
